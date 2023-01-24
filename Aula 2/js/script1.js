e9f16a2990096058460c97b5035b8ab6d503c93d
Maurício Medeiros <mauricioms@gmail.com>
2023-01-24 17:34:56 -0300

    Criação do script2 e index1 com a inserção de alguns exercícios

---
+++ Aula 2/js/script1.js
@@ -0,0 +1,30 @@
+function corFundo() {
+  var cor = document.getElementById("txtCor").value;
+  cor = cor.toLowerCase(); //COLOCA TODAS AS LETRAS EM MINÚSCULA
+
+  if (cor == "red") {
+    document.body.style.backgroundColor = "#FF0000";
+  } 
+  else if (cor == "black") {
+    document.body.style.backgroundColor = "#000000";
+  } 
+  else if (cor == "green") {
+    document.body.style.backgroundColor = "#00FF00";
+  } 
+  else {
+    document.body.style.backgroundColor = "#FFFFFF";
+    alert("A cor não existe na paleta!!");
+  }
+}
+
+
+function textoClaro(){
+    document.getElementsByTagName("h1")[0].style.color = "white";
+    document.getElementsByTagName("label")[0].style.color = "white";
+}
+
+function textoEscuro(){
+    document.getElementsByTagName("h1")[0].style.color = "darkred";
+    document.getElementsByTagName("label")[0].style.color = "darkred";
+}
+
---
+++ Aula 2/js/script2.js
@@ -0,0 +1,37 @@
+function ola() {
+  console.log("Hello world!");
+}
+
+function bar() {
+  var foo = "bar";
+  console.log(foo);
+}
+
+function escolhaVar() {
+  var thisVar = "first value";
+  var thatVar = "second value";
+  console.log("thisVar: ", thisVar, "and thatVar: ", thatVar);
+}
+
+// Placeholder. You can use console.log() in combination with placeholders:
+
+var greet = "Hello",
+  who = "World";
+console.log("%s, %s!", greet, who);
+
+console.log({
+  Email: "",
+  Groups: "",
+  Id: 33,
+  IsSiteAdmin: false,
+  IsHiddenInUI: false,
+  LoginName: "i:#.w|virtualdomain\\user2",
+  PrincipalType: 1,
+  Title: "user2",
+});
+
+console.log("Hello World!");
+var x = 10;
+var y = 20;
+var sum = x + y;
+console.log(sum);
