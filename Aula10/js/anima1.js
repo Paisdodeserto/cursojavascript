e62e24f3f030f961dd196109c7b5056de29cbae8
MAURICIO MEDEIROS DA SILVA <mauricio.msilva21@senacsp.edu.br>
2023-02-23 17:38:32 -0300

    Trabalhando com animações.

---
+++ Aula10/js/anima1.js
@@ -0,0 +1,89 @@
+// function dizOi(){
+//     console.log("Oi");
+// }
+// window.setInterval(dizOi, 2000); 
+
+
+// window.setInterval(()=>{
+//     console.log("Oi");
+// },2000);
+
+
+// let x = 1;
+// window.setInterval(()=>{
+//     console.log(`Contei ${x}`);
+//     x++;
+// },1000);
+
+
+const quadro = document.querySelector('#quadro');
+// let x = 50;
+// let r = 10;
+// window.setInterval(()=>{
+//     //alterar os estilos de largura e altura do quadro.
+//     x++;
+//     quadro.style.width = `${x}px`;
+//     quadro.style.height = `${x}px`;
+//     quadro.style.transform = `rotate(${r}deg)`;
+//     quadro.style.borderRadius = `${r/2}px`;
+//     r++;
+// }, 100);
+
+
+// let pY = -500;
+
+// window.setInterval(()=>{
+//     console.log(quadro.style.top);
+//     quadro.style.top = `${pY}px`;
+//     if(pY >= 10){
+//         pY = 10;
+//     }
+//     pY+=5;
+// }, 100);
+
+
+// let t = -500;
+// const quadroA = window.setInterval(()=>{
+//     console.log(quadro.style.top);
+//     quadro.style.top = `${t}px`;
+//     if(t >= 10){
+//         clearInterval(quadroA);
+//     }
+//     t += 2;
+// }, 20);
+
+
+// window.document.body.onmousemove = () =>{
+//     console.log(event.clientX, event.clientY); //pega a posição do mouse em toda a tela
+// };
+
+// let pX = 0;
+// let pY = 0;
+
+// document.body.onclick = (event) =>{
+
+//     const mov = window.setInterval(()=>{        
+
+//         if (pX == event.clientX){
+//             clearInterval(mov);
+//         }
+        
+//         quadro.style.position = "absolute";
+//         quadro.style.top = `${event.clientY - pX}px`;
+//         quadro.style.left = `${event.clientX - pY}px`;
+    
+//     }, 20)};
+
+
+// function relogio (){
+//     var tempo = new Date();
+//     var hora = tempo.getHours();
+//     var minuto = tempo.getMinutes();
+//     var segundo = tempo.getSeconds();
+
+//     quadro.innerHTML = `${hora}:${minuto}:${segundo}`;
+    
+// }
+// window.setInterval(relogio, 1000);
+
+
---
+++ Aula10/js/anima2.js
@@ -0,0 +1,10 @@
+abrir.onclick= () => {
+    // console.log("clicou");
+    fundo.style.backgroundColor = "rgba(0,0,0,0.5)";
+    tela.style.top = "0px";
+};
+
+fechar.onclick= () => {    
+    fundo.style.backgroundColor = "rgba(0,0,0,0)";
+    tela.style.top = "-600px";
+};
