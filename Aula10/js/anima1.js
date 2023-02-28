45c85d2847236dae3be25e6dcf539b74182edd8f
MAURICIO MEDEIROS DA SILVA <mauricio.msilva21@senacsp.edu.br>
2023-02-28 17:30:52 -0300

    Comentários e um novo arquivo na Aula 10. Aula 11 animação de menu, de rolagem de página, consulta em api com uso de fetch.

--- Aula10/js/anima1.js
+++ Aula10/js/anima1.js
@@ -1,89 +1,72 @@
-// function dizOi(){
-//     console.log("Oi");
+// function dizOi() {
+//   console.log("Oi");
 // }
-// window.setInterval(dizOi, 2000); 
+// window.setInterval(dizOi, 2000);
 
-
-// window.setInterval(()=>{
-//     console.log("Oi");
-// },2000);
-
+// window.setInterval(() => {
+//   console.log("Oi");
+// }, 2000);
 
 // let x = 1;
-// window.setInterval(()=>{
-//     console.log(`Contei ${x}`);
-//     x++;
-// },1000);
+// window.setInterval(() => {
+//   console.log(`Contei ${x}`);
+//   x++;
+// }, 1000);
 
-
-const quadro = document.querySelector('#quadro');
+const quadro = document.querySelector("#quadro");
 // let x = 50;
 // let r = 10;
-// window.setInterval(()=>{
-//     //alterar os estilos de largura e altura do quadro.
-//     x++;
-//     quadro.style.width = `${x}px`;
-//     quadro.style.height = `${x}px`;
-//     quadro.style.transform = `rotate(${r}deg)`;
-//     quadro.style.borderRadius = `${r/2}px`;
-//     r++;
+// window.setInterval(() => {
+//   //alterar os estilos de largura e altura do quadro
+//   x++;
+//   quadro.style.width = `${x}px`;
+//   quadro.style.height = `${x}px`;
+//   quadro.style.transform = `rotate(${r}deg)`;
+//   quadro.style.borderRadius = `${r / 2}px`;
+//   r++;
 // }, 100);
-
 
 // let pY = -500;
 
-// window.setInterval(()=>{
-//     console.log(quadro.style.top);
-//     quadro.style.top = `${pY}px`;
-//     if(pY >= 10){
-//         pY = 10;
-//     }
-//     pY+=5;
-// }, 100);
-
-
-// let t = -500;
-// const quadroA = window.setInterval(()=>{
-//     console.log(quadro.style.top);
-//     quadro.style.top = `${t}px`;
-//     if(t >= 10){
-//         clearInterval(quadroA);
-//     }
-//     t += 2;
+// window.setInterval(() => {
+//   console.log(quadro.style.top);
+//   quadro.style.top = `${pY}px`;
+//   if (pY >= 10) {
+//     pY = 10;
+//   }
+//   pY += 2;
 // }, 20);
 
-
-// window.document.body.onmousemove = () =>{
-//     console.log(event.clientX, event.clientY); //pega a posição do mouse em toda a tela
-// };
+// let t = -500;
+// const quadroA = window.setInterval(() => {
+//   console.log(quadro.style.top);
+//   quadro.style.top = `${t}px`;
+//   if (t >= 10) {
+//     window.clearInterval(quadroA);
+//   }
+//   t += 2;
+// }, 20);
 
 // let pX = 0;
 // let pY = 0;
 
-// document.body.onclick = (event) =>{
+// document.body.onclick = (event) => {
+//   const mov = window.setInterval(() => {
+//     if (pX == event.clientX) {
+//       clearInterval(mov);
+//     }
+//     quadro.style.position = "absolute";
+//     quadro.style.top = `${event.clientY - pY}px`;
+//     quadro.style.left = `${event.clientX - pX}px`;
+//   }, 20);
+// };
 
-//     const mov = window.setInterval(()=>{        
+// function relogio() {
+//   var tempo = new Date();
+//   var hora = tempo.getHours();
+//   var minuto = tempo.getMinutes();
+//   var segundo = tempo.getSeconds();
 
-//         if (pX == event.clientX){
-//             clearInterval(mov);
-//         }
-        
-//         quadro.style.position = "absolute";
-//         quadro.style.top = `${event.clientY - pX}px`;
-//         quadro.style.left = `${event.clientX - pY}px`;
-    
-//     }, 20)};
-
-
-// function relogio (){
-//     var tempo = new Date();
-//     var hora = tempo.getHours();
-//     var minuto = tempo.getMinutes();
-//     var segundo = tempo.getSeconds();
-
-//     quadro.innerHTML = `${hora}:${minuto}:${segundo}`;
-    
+//   quadro.innerHTML = `${hora}:${minuto}:${segundo}`;
 // }
 // window.setInterval(relogio, 1000);
-
-
